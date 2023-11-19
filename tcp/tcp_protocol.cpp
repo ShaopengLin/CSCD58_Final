2b27ce5b896652ac011e904aebbb65c14bbb9fb4
None <None>
2023-11-18 21:14:24 -0800

    Added intial tcp header definition files

---
+++ tcp/tcp_protocol.cpp
@@ -0,0 +1,7 @@
+#include "tcp_protocol.h"
+
+/* TODO: Method to create tcp packet */
+
+/* TODO: Method to print debug view of tcp packet */
+
+/* TODO: Method to verify tcp header */
---
+++ tcp/tcp_protocol.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+#ifndef TCP_PROTOCOL_H
+#define TCP_PROTOCOL_H
+
+/* TODO: Macros for flags, attributes in hdr, timeout, etc*/
+
+/* TODO: Macros to handle Endian */
+
+struct tcp_hdr
+{
+
+} __attribute__ ((packed));
+
+#endif /* -- TCP_PROTOCOL_H -- */
